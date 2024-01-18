df39e8610af563d793f120a71715d69a769177a2
이연정 <lyj7877@naver.com>
2024-01-18 21:52:58 +0900

    [feat] pages > home 레이아웃 및 css 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,45 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-    return <h1>HOME PAGE</h1>;
+    return (
+        <div>
+            <header>
+                <div>
+                    <Link to="/post/new">글쓰기</Link>
+                    <Link to="/posts">게시글</Link>
+                    <Link to="/profile">프로필</Link>
+                </div>
+            </header>
+            <div className="post__navigation">
+                <div className="post__navigation--active">전체</div>
+                <div>나의 글</div>
+            </div>
+            <div className="post__list">
+                {[...Array(10)].map((e, index) => (
+                    <div key={index} className="post__box">
+                        <Link to={`/posts/${index}`}>
+                            <div className="post__profile-box">
+                                <div className="post__profile"></div>
+                                <div className="post__author-name">비연쎄</div>
+                                <div className="post__date">2024.01.19</div>
+                            </div>
+                            <div className="post__title">게시글 {index}</div>
+                            <div className="post__text">
+                                행복한 날입니다.
+                            </div>
+                            <div className="post__utils-box">
+                                <div className="post__delete"></div>
+                                <div className="post__edit"></div>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+            <footer>
+                <Link to="/post/new">글쓰기</Link>
+                <Link to="/posts">게시글</Link>
+                <Link to="/profile">프로필</Link>
+            </footer>
+        </div>
+    )
 }
