c46d9ae05dd08a2c0feb0b41a59fbaf56226bc6b
이연정 <lyj7877@naver.com>
2024-01-18 23:34:32 +0900

    [style] 불필요한 div 삭제

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -3,19 +3,17 @@ export default function PostDetail() {
         <div className="post__detail">
             <div className="post__box">
                 <div className="post__title">title</div>
-                <div>
-                    <div className="post__profile-box">
-                        <div className="post__profile"></div>
-                        <div className="post__author-name">비연쎄</div>
-                        <div className="post__date">2024.01.19</div>
-                    </div>
-                    <div className="post__utils-box">
-                        <div className="post__delete">삭제</div>
-                        <div className="post__edit">수정</div>
-                    </div>
-                    <div className="post__text">
-                        행복한 날입니다.
-                    </div>
+                <div className="post__profile-box">
+                    <div className="post__profile"></div>
+                    <div className="post__author-name">비연쎄</div>
+                    <div className="post__date">2024.01.19</div>
+                </div>
+                <div className="post__utils-box">
+                    <div className="post__delete">삭제</div>
+                    <div className="post__edit">수정</div>
+                </div>
+                <div className="post__text">
+                    행복한 날입니다.
                 </div>
             </div>
         </div>
