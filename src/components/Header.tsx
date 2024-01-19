65db23def94edb5bad32b8a3bb9e48b684bcb0e4
이연정 <lyj7877@naver.com>
2024-01-19 17:42:33 +0900

    [fix] component > header 경로 오탈자 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
             <header className="header">
                 <Link to="/" className="header__logo">Study Log</Link>
                 <div>
-                    <Link to="/post/new">글쓰기</Link>
+                    <Link to="/posts/new">글쓰기</Link>
                     <Link to="/posts">게시글</Link>
                     <Link to="/profile">프로필</Link>
                 </div>
