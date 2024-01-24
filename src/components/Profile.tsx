0fae848e2a4fe99a0485c7623cfd63157a7f1cda
이연정 <lyj7877@naver.com>
2024-01-24 22:13:33 +0900

    [feat] useContext 사용해 auth 가져오기

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,8 @@
+import AuthContext from "context/AuthContext"
 import {app} from "firebaseApp"
 import {getAuth, signOut} from "firebase/auth";
 import {toast} from 'react-toastify'
+import { useContext } from "react";
 
 const onSignOut = async () => {
     try {
@@ -15,15 +17,16 @@ const onSignOut = async () => {
 }
 
 export default function Profile() {
-    const auth = getAuth(app);
+    const {user} = useContext(AuthContext);
 
+ 
     return (
         <div className="profile__box">
             <div className="flex__box-lg">
                 <div className="profile__image" />
                 <div>
-                    <div className="profile__email">{auth?.currentUser?.email}</div>
-                    <div className="profile__name">{auth?.currentUser?.displayName || "사용자"}</div>
+                    <div className="profile__email">{user?.email}</div>
+                    <div className="profile__name">{user?.displayName || "사용자"}</div>
                 </div>
                 <div role="presentation" className="profile__logout" onClick={onSignOut}>
                     로그아웃
