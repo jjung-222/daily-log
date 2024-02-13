cd874b90f9a95aae25d96da3e15fbc8c0f1de347
이연정 <lyj7877@naver.com>
2024-02-13 22:39:41 +0900

    [feat] 글 삭제 및 카테고리 기능 추가

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react"
-import { Link, useParams } from "react-router-dom";
-import { doc, getDoc } from "firebase/firestore"
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { doc, getDoc, deleteDoc } from "firebase/firestore"
 import { db } from "firebaseApp"
 import { PostProps } from "./PostList"
 import Loader from "./Loader";
+import {toast} from "react-toastify"
 
 export default function PostDetail() {
     const [post, setPost] = useState<PostProps | null>(null);
     const params = useParams();
+    const navigate = useNavigate();
 
     const getPost = async (id: string) => {
         if (id) {
@@ -18,8 +20,12 @@ export default function PostDetail() {
         }
     };
 
-    const handleDelete = () => {
-
+    const handleDelete = async () => {
+        const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+        if(confirm && post && post.id) {
+            await deleteDoc(doc(db, "posts", post.id));
+            toast.success("게시글을 삭제했습니다.");
+        }
     }
 
     useEffect(() => {
@@ -39,8 +45,11 @@ export default function PostDetail() {
                         <div className="post__date">{post?.createdAt}</div>
                     </div>
                     <div className="post__utils-box">
+                        {post?.category && 
+                            <div className="post__category">{post?.category}</div>
+                        }
                         <div className="post__delete" role="presentation" onClick={handleDelete}>삭제</div>
-                        <div className="post__edit"><Link to={`/posts/edit/1`}>수정</Link></div>
+                        <div className="post__edit"><Link to={`/posts/edit/${post?.id}`}>수정</Link></div>
                     </div>
                     <div className="post__text post__text--pre-wrap">
                         {post?.content}
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,15 +1,15 @@
 import { useContext } from "react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore"
+import { collection, getDocs, deleteDoc, doc, query, orderBy, where } from "firebase/firestore"
 import { db } from "firebaseApp"
 import AuthContext from "context/AuthContext"
+import {toast} from "react-toastify"
 
 interface PostListProps {
     hasNavigation?: boolean;
+    defaultTab?: TabType | CategoryType;
 }
-
-type TabType = "all" | "my";
 
 export interface PostProps {
     id?: string,
@@ -18,15 +18,39 @@ export interface PostProps {
     summary: string,
     content: string,
     createdAt: string,
+    updatedAt?: string,
+    uid: string,
+    category?: CategoryType,
 }
 
-export default function PostList({ hasNavigation = true }: PostListProps) {
-    const [activeTab, setActiveTab] = useState<TabType>("all")
+type TabType = "all" | "my";
+export type CategoryType = "Frontend" | "Backend" | "Web" | "Native";
+export const CATEGORIES: CategoryType[] = [
+    "Frontend", 
+    "Backend", 
+    "Web", 
+    "Native",
+]
+
+export default function PostList({ hasNavigation = true, defaultTab = "all" }: PostListProps) {
+    const [activeTab, setActiveTab] = useState<TabType | CategoryType>(defaultTab)
     const [posts, setPosts] = useState<PostProps[]>([]);
     const { user } = useContext(AuthContext);
 
     const getPosts = async () => {
-        const datas = await getDocs(collection(db, "posts"));
+        setPosts([]);
+        let postsRef = collection(db, "posts");
+        let postsQuery = query(postsRef, orderBy("createdAt", "asc"));
+
+        if(activeTab === "my" && user) {
+            postsQuery = query(postsRef, where("uid", "==", user.uid), orderBy("createdAt", "asc"));
+        } else if(activeTab === "all") {
+            postsQuery = query(postsRef, orderBy("createdAt", "asc"));
+        } else {
+            postsQuery = query(postsRef, where("category", "==", activeTab), orderBy("createdAt", "asc"));
+        }
+
+        const datas = await getDocs(postsQuery);
         datas?.forEach((doc) => {
             console.log(doc.data())
             const dataObj = { ...doc.data(), id: doc.id };
@@ -34,9 +58,19 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
         })
     }
 
+    const handleDelete = async (id: string) => {
+        const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+        if(confirm && id) {
+            await deleteDoc(doc(db, "posts", id));
+            toast.success("게시글을 삭제했습니다.");
+            getPosts()
+        }
+    
+    }
+
     useEffect(() => {
         getPosts()
-    }, [])
+    }, [activeTab])
 
     return (
         <>
@@ -55,6 +89,16 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
                     >
                         나의 글
                     </div>
+                    {CATEGORIES?.map((category)=>(
+                    <div
+                        key={category}
+                        role="presentation"
+                        onClick={() => setActiveTab(category)}
+                        className={activeTab === category ? "post__navigation--active" : ""}
+                    >
+                        {category}
+                    </div>
+                    ))}
                 </div>
             )}
 
@@ -75,7 +119,7 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
                         {
                             post?.email === user?.email && (
                                 <div className="post__utils-box">
-                                    <div className="post__delete">삭제</div>
+                                    <div className="post__delete" role="presentation" onClick={() => handleDelete(post.id as string)}>삭제</div>
                                     <div className="post__edit">
                                         <Link to={`/posts/edit/${post?.id}`}>수정</Link>
                                     </div>
